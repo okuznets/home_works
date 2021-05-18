4fe1fc1ba05fe7038849dcc93f917e7b78ab5eb3
fesenkoaa <fesenko.a@icloud.com>
2021-05-18 14:34:28 +0300

    Homework 1

--- andrewfesenko/homework1.py
+++ andrewfesenko/homework1.py
@@ -5,12 +5,12 @@ name = input('My name is:')
 print(name, ', enter your age.')
 age = input('My age is:')
 print('Who are you:')
-sex = input('My gender is:') #1 (done)
+sex = input('My gender is:')    #1 (done)
 
-about_me_fstring = ('Hello, my name is ' +  name + '. I\'m ' + age + ' and I\'m a ' + sex + '.')    #2 (done)
+about_me_fstring = 'Hello, my name is ' +  name + '. I\'m ' + age + ' and I\'m a ' + sex + '.'    #2 (done)
 print(about_me_fstring)
 
-first = ('Hello, my name is %s' % name + '. I\'m %s' % age + ' and I\'m a %s' % sex + '.')  #3 (done)
+first = 'Hello, my name is %s. I\'m %s and I\'m a %s.' % (name, age, sex)    #3 (done)
 print(first)
 
 second = 'Hello, my name is {0}. I\'m {1} and I\'m a {2}.'.format(name, age, sex)    #4 (done)
@@ -27,15 +27,17 @@ print(name, ', enter your friend\'s age.')
 age1 = input('His/Her age:')
 print('Who is your friend:')
 sex1 = input('His/Her gender:')
-about_me_fstring = f'He/She is {name1}. He/She is {age1} and he/she is a {sex1}.' #7 (done)
-print(about_me_fstring)
+about_me_fstring = about_me_fstring.replace(name, name1)
+about_me_fstring = about_me_fstring.replace(age, age1)
+about_me_fstring = about_me_fstring.replace(sex, sex1)
+print(about_me_fstring)    #7 (done)
 
 list_from_str = list(about_me_fstring)    #8 (done)
 print(list_from_str)
 
 print(type(list_from_str))  #9 (done)
 
-str_from_list = ''.join(list_from_str)   #10 (done)
+str_from_list = ''.join(list_from_str)      #10 (done)
 print(str_from_list)
 
 print(about_me_fstring.swapcase())    #11 (done)
