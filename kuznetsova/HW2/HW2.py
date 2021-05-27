add04bf4f26b1000a497a44c9d9452f9d7e9bced
Olena Kuznetsova <cheredaky@gmail.com>
2021-05-27 16:38:08 +0300

    Home work 2 + labs (lesson 8)//use len only for pp8-9. and no idea for code unification for p.7 ((

--- kuznetsova/HW2/HW2.py
+++ kuznetsova/HW2/HW2.py
@@ -38,13 +38,13 @@ sample_dict['class_b']['student2']['marks']['physics'] = 60
 print(sample_dict)
 # 6. Подсчитать средний бал по каждому студенту (результат округлить до 2 знаков после запятой);
 student1a_marks = list(sample_dict['class_a']['student1']['marks'].values())
-student1a_avg = sum(student1a_marks) / len(student1a_marks)
+student1a_avg = round(sum(student1a_marks) / len(student1a_marks),2)
 student2a_marks = list(sample_dict['class_a']['student2']['marks'].values())
-student2a_avg = sum(student2a_marks) / len(student2a_marks)
+student2a_avg = round(sum(student2a_marks) / len(student2a_marks),2)
 student1b_marks = list(sample_dict['class_b']['student1']['marks'].values())
-student1b_avg = sum(student1b_marks) / len(student1b_marks)
+student1b_avg = round(sum(student1b_marks) / len(student1b_marks),2)
 student2b_marks = list(sample_dict['class_b']['student2']['marks'].values())
-student2b_avg = sum(student2b_marks) / len(student2b_marks)
+student2b_avg = round(sum(student2b_marks) / len(student2b_marks),2)
 
 # 7. Создать словарь со средним баллом за каждого студента;
 avg_marks_dict = {sample_dict['class_a']['student1']['name']:student1a_avg,
@@ -58,12 +58,12 @@ max_mark = max(avg_marks_dict.values())
 best_student = list(avg_marks_dict.keys())[list(avg_marks_dict.values()).index(max_mark)]
 print(f'the best student is {best_student} ')
 # 9. Подсчитать средний бал по каждому классу (результат округлить до 2 знаков после запятой);
-class_a_avg = round((student1a_avg + student2a_avg) /2,2)
-class_b_avg = round((student1b_avg + student2b_avg) /2,2)
+class_a_avg = round((student1a_avg + student2a_avg) /len(sample_dict['class_a']),2)
+class_b_avg = round((student1b_avg + student2b_avg) /len(sample_dict['class_b']),2)
 # 8. Создать словарь со средним баллом за классы;
 avg_class_dict = {'class_a': class_a_avg, 'class_b': class_b_avg}
 print(avg_class_dict)
 # 9. Определить лучший класс по успеваемости.
 max_mark_class = max(avg_class_dict.values())
 best_class = list(avg_class_dict.keys())[list(avg_class_dict.values()).index(max_mark_class)]
-print(f'the best class is {best_class} ')
+print(f'the best class is {best_class}')
